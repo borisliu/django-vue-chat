1531891ad72b33add3c5e0f1404d6595df33badd
Boris Liu <boris_cn@263.net>
2018-05-22 11:52:44 +0800

    完成Django程序的设置

--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
 from django.urls import path
 
-websocket_urlpatterns = [
+from . import consumers
 
+websocket_urlpatterns = [
+    path('ws/client/<str:room_name>/', consumers.ClientConsumer),
+    path('ws/service/<str:room_name>/', consumers.ServiceConsumer),
 ]
